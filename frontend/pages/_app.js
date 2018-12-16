91f09a0f2ca4ca10da2de8cc7e310647cd738e2c
wajdi kaznali <wajdi.kanzali@oyez.fr>
2018-12-17 00:20:59 +0100

    delete unuseful text

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,7 +7,6 @@ class MyApp extends App {
 
         return (
             <Container>
-                <p>Heey!</p>
                 <Page>
                     <Component />
                 </Page>
