b58d5f71abb4d788e03deb630386298b119e0cb5
wajdi kaznali <wajdi.kanzali@oyez.fr>
2019-02-23 22:30:02 +0100

    add mutation to update item && delete item

--- back/src/resolvers/Mutation.js
+++ back/src/resolvers/Mutation.js
@@ -1,13 +1,27 @@
 const Mutations = {
-    async createItem(_, args, ctx, info) {
-         const item = await ctx.db.mutation.createItem({
-           data: {
-            ...args
-          }
-        }, info);
-        return item;
-     }
-   };
-   
+  async createItem(_, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    return item;
+  },
+
+  async updateItem(_, args, ctx, info) {
+    const { id, ...rest } = args;
+    const item = await ctx.db.mutation.updateItem({
+      data: rest,
+      where: { id },
+    }, info);
+    return item;
+  },
+
+  async deleteItem(_, args, ctx, info) {
+    const where = { id: args.id };
+    const item = await ctx.db.query.item({ where }, info);
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+};
+
 module.exports = Mutations;
-   
