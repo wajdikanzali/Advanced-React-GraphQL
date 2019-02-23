6f5baeae1e9a56521449dcf5247e883dcac31184
wajdi kaznali <wajdi.kanzali@oyez.fr>
2019-02-23 23:09:12 +0100

    fix pagination

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,19 +4,23 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 
 const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+        items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
             id
             title
             price
             description
             image
             largeImage
-        }
-    }
+        },
+    },
+    {
+    options: { fetchPolicy: 'network-only' },
+  }
 `;
 
 const Center = styled.div`
@@ -36,7 +40,13 @@ class Items extends Component {
         return (
             <Center>
                 <Pagination page={ this.props.page }/>
-                <Query query={ALL_ITEMS_QUERY}>
+                <Query 
+                    query={ALL_ITEMS_QUERY}
+                    // fetchPolicy='network-only'
+                    variables={{
+                        skip: this.props.page * perPage - perPage,
+                    }}
+                >
                 {({ data, error, loading }) => {
                     if (loading) return <p>loading...</p>
                     if (error) return <p>Error: {error.message} </p>;
