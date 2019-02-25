9ffe2a53f14c22577e3c91ce705ec2a095256fd0
wajdi kaznali <wajdi.kanzali@oyez.fr>
2019-02-25 16:41:03 +0100

    Signin form && permession nav

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,21 +3,16 @@ import NavStyles from './styles/NavStyles';
 import User from './User';
 
 const Nav = () => (
-    <NavStyles>
-        <User>
-            {({ data: { me } }) => {
-                if (me) return <p>{me.name}</p>;
-                return null;
-            }}
-        </User>
+    <User>
+         {({ data: { me } }) => (
+       <NavStyles>
         <Link href="/items">
             <a>Shop</a>
         </Link>
+        {me && (
+            <>
         <Link href="/sell">
             <a>Sell</a>
-        </Link>
-        <Link href="/signup">
-            <a>Signup</a>
         </Link>
         <Link href="/ordres">
             <a>Ordres</a>
@@ -25,7 +20,16 @@ const Nav = () => (
         <Link href="/me">
             <a>Account</a>
         </Link>
+        </>
+        )}
+        {!me && (
+        <Link href="/signup">
+            <a>Signup</a>
+        </Link>
+        )}
     </NavStyles>
-)
+       )}
+    </User>
+);
 
 export default Nav;
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,4 +1,5 @@
 import Signup from '../components/Signup';
+import Signin from '../components/Signin';
 import styled from 'styled-components';
 
 const Columns = styled.div`
@@ -10,6 +11,7 @@ const Columns = styled.div`
 const signup = props => (
     <Columns>
         <Signup />
+        <Signin />
     </Columns>
 )
 
