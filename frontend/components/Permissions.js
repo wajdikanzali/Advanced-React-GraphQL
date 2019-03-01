2faff8a57553adfe125a38968149e77cad3844a4
wajdi kaznali <wajdi.kanzali@oyez.fr>
2019-03-02 00:11:07 +0100

    Permissions managment && Updating Permissions in Local State

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import { Query } from 'react-apollo';
 import Error from './ErrorMessage';
 import gql from 'graphql-tag';
-import UsersWrapper from './styles/UsersWrapper';
+import styled from 'styled-components';
+import Table from './styles/Table';
+import Form from './styles/Form';
 import SickButton from './styles/SickButton';
-import { Logo, Tips } from "./styles/Utils";
+import PropTypes from 'prop-types';
 
-import ReactTable from 'react-table';
+const Columns = styled.div`
+  padding: 5px;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
 
 const possiblePermissions = [
   'ADMIN',
@@ -37,75 +43,148 @@ const Permissions = (props) => (
           <Error error={error} />
           <div>
             <h2>Manage Permissions</h2>
-            <User users={data.users} possiblePermissions={possiblePermissions} />
+            {data.users.map(user => <UserPermissions user={user} key={user.id} />)}
           </div>
         </div>)
     }}
   </Query>
 );
 
-class User extends React.Component {
+class UserPermissions extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.array,
+    }).isRequired,
+  };
+  state = {
+    permissions: this.props.user.permissions,
+  };
+  handlePermissionChange = (e) => {
+    const checkbox = e.target;
+    let updatedPermissions = [...this.state.permissions];
+    if (checkbox.checked) {
+      updatedPermissions.push(checkbox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(permission => permission !== checkbox.value);
+    }
+    this.setState({ permissions: updatedPermissions });
+  };
   render() {
-    const action = {
-      columns: [
-        {
-          Header: 'ACTION',
-          width: 75,
-          Cell: ({ original }) => {
-            return (
-              <div>
-                {
-                  <button
-                    onClick={() => { this.openVideos(original); }}
-                    className="pencil"
-                    title="Update"
-                  />
-                }
-              </div>
-            );
-          },
-        },
-      ],
-    };
-    const permessions = (this.props.possiblePermissions).map(permission => ({
-      Header: permission,
-      width: 80,
-      Cell: ({ original }) => {
-        return (
-          <div>
-            {
-              <label htlmfor={`${original.id}-permission-${permission}`}>
-                <input type='checkbox' />
-              </label>
-            }
-          </div>
-        );
-      },
-    }))
-    const columns = [
-      {
-        Header: 'Name',
-        accessor: 'name'
-      },
-      {
-        Header: 'Email',
-        accessor: 'email',
-        width: 250,
-      },
-      ...permessions,
-      action
-    ];
-
+    const { user } = this.props;
     return (
-      <UsersWrapper>
-        <ReactTable
-          columns={columns}
-          data={this.props.users}
-        >
-        </ReactTable>
-      </UsersWrapper>
+      <Columns>
+        <Form>
+          <fieldset>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  {possiblePermissions.map(permission => <th>{permission}</th>)}
+                  <th>ACTION</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  {possiblePermissions.map(permission => (
+                    <td>
+                      <label htlmfor={`${permission.id}-permission-${permission}`}>
+                        <input
+                          id={`${user.id}-permission-${permission}`}
+                          type="checkbox"
+                          checked={this.state.permissions.includes(permission)}
+                          value={permission}
+                          onChange={this.handlePermissionChange}
+                        />
+                      </label>
+                    </td>
+                  ))}
+                  <td>
+                    <SickButton>Update</SickButton>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </fieldset>
+        </Form>
+      </Columns>
     )
   }
 }
+
+// class User extends React.Component {
+//   render() {
+//     const action = {
+//       columns: [
+//         {
+//           Header: 'ACTION',
+//           width: 75,
+//           Cell: ({ original }) => {
+//             return (
+//               <div>
+//                 {
+//                   <button
+//                     className="pencil"
+//                     title="Update"
+//                   />
+//                 }
+//               </div>
+//             );
+//           },
+//         },
+//       ],
+//     };
+//     const permessions = (this.props.possiblePermissions).map(permission => ({
+//       Header: permission,
+//       width: 80,
+//       Cell: ({ original }) => {
+//         return (
+//           <div>
+//             {
+//               <label htlmfor={`${original.id}-permission-${permission}`}>
+//                 <input
+//                   id={`${original.id}-permission-${permission}`}
+//                   type='checkbox'
+//                   // checked={this.state.selected[`${original.id}-permission-${permission}`] === true}
+//                   checked={original.permissions.includes(permission)}
+//                   value={permission}
+//                   onChange={() => this.handlePermissionChange(`${original.id}-permission-${permission}`)}
+//                 />
+//               </label>
+//             }
+//           </div>
+//         );
+//       },
+//     }))
+//     const columns = [
+//       {
+//         Header: 'Name',
+//         accessor: 'name'
+//       },
+//       {
+//         Header: 'Email',
+//         accessor: 'email',
+//         width: 250,
+//       },
+//       ...permessions,
+//       action
+//     ];
+
+//     return (
+//       <UsersWrapper>
+//         <ReactTable
+//           columns={columns}
+//           data={this.props.users}
+//         >
+//         </ReactTable>
+//       </UsersWrapper>
+//     )
+//   }
+// }
 
 export default Permissions;
