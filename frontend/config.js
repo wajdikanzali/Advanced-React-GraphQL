a9b6a672068e17fb9138f279fe4a42902d829e9b
wajdi kaznali <wajdi.kanzali@oyez.fr>
2019-03-14 00:44:37 +0100

    push config prod

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:7000`;
+export const prodEndpoint = `https://adidas-apollo-prod.herokuapp.com/`;
 export const perPage = 4;
