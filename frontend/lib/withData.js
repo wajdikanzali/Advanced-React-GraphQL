16b291e50b14a011bfe22f9e3ff9aa365cb83262
wajdi kaznali <wajdi.kanzali@oyez.fr>
2019-03-14 15:30:49 +0100

    init config

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -5,7 +5,7 @@ import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
